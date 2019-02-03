277d676da5fac7c5564f183d6a9d08a9eb31edd0
Nikolai <ni-sirius@ya.ru>
2019-02-03 21:30:52 +0400

    Full lightning with shaders

--- OpenGLConsole/vertex_core.glsl
+++ OpenGLConsole/vertex_core.glsl
@@ -3,10 +3,12 @@
 layout (location = 0) in vec3 vertex_position;
 layout (location = 1) in vec3 vertex_color;
 layout (location = 2) in vec2 vertex_texcoord;
+layout (location = 3) in vec3 vertex_normal;
 
 out vec3 vs_position;
 out vec3 vs_color;
 out vec2 vs_texcoord;
+out vec3 vs_normal;
 
 uniform mat4 ModelMat;
 uniform mat4 ViewMat;
@@ -17,6 +19,7 @@ void main()
 	vs_position =  vec4(ModelMat * vec4(vertex_position, 1.f)).xyz;
 	vs_color = vertex_color;
 	vs_texcoord = vec2(vertex_texcoord.x, vertex_texcoord.y * -1.f);
+	vs_normal = mat3(ModelMat) * vertex_normal;
 
 	gl_Position = ProjectionMat * ViewMat * ModelMat * vec4(vertex_position, 1.f);
 }
