b5937296a11bec9a703bdfc57c66d367e5ff570f
Nikolai <ni-sirius@ya.ru>
2019-02-03 18:55:32 +0400

    Added: work with matrices

--- OpenGLConsole/vertex_core.glsl
+++ OpenGLConsole/vertex_core.glsl
@@ -8,11 +8,15 @@ out vec3 vs_position;
 out vec3 vs_color;
 out vec2 vs_texcoord;
 
+uniform mat4 ModelMat;
+uniform mat4 ViewMat;
+uniform mat4 ProjectionMat;
+
 void main()
 {
-	vs_position = vertex_position;
+	vs_position =  vec4(ModelMat * vec4(vertex_position, 1.f)).xyz;
 	vs_color = vertex_color;
 	vs_texcoord = vec2(vertex_texcoord.x, vertex_texcoord.y * -1.f);
 
-	gl_Position = vec4(vertex_position, 1.f);
+	gl_Position = ProjectionMat * ViewMat * ModelMat * vec4(vertex_position, 1.f);
 }
