0f8c2b13f47ec5f3c700cbd80ef2fb3a883125a7
Nikolai <ni-sirius@ya.ru>
2019-02-09 09:49:08 +0400

    Added: light calculations divided into a functions

--- OpenGLConsole/fragment_core.glsl
+++ OpenGLConsole/fragment_core.glsl
@@ -17,32 +17,39 @@ in vec3 vs_normal;
 out vec4 fs_color;
 
 uniform Material material;
-
 uniform vec3 LightPos0;
 uniform vec3 CameraPos;
 
-void main()
+vec3 calculateAmbient(Material material)
 {
-  //fs_color = vec4(vs_color, 1.f);
-  //Ambient light
-  vec3 ambientLight = material.ambient;
-
-  //diffuse light
+  return material.ambient;
+}
+vec3 calculateDiffuse(Material material, vec3 vs_position, vec3 vs_normal, vec3 LightPos0)
+{
   vec3 posToLightDirVec = normalize(vs_position - LightPos0);
   //vec3 diffuseColor = vec3(1.f, 1.f, 1.f);
   float diffuse = clamp(dot(posToLightDirVec, vs_normal), 0, 1);
-  vec3 diffuseFinal = material.diffuse * diffuse;
-
-  //specular
+  return  material.diffuse * diffuse;
+}
+vec3 calculateSpecular(Material material, vec3 vs_position, vec3 vs_normal, vec3 LightPos0, vec3 CameraPos)
+{
   vec3 lightToPosDirVec = normalize(LightPos0 - vs_position);
   vec3 reflectDirVec = normalize(reflect(lightToPosDirVec, normalize(vs_normal)));
   vec3 posToViewDirVec = normalize(vs_position - CameraPos);
   float specularConstant = pow(max(dot(posToViewDirVec, reflectDirVec), 0), 100);
-  vec3 specularFinal = material.specular * specularConstant;
+  return material.specular * specularConstant;
+}
+
+void main()
+{
+  //fs_color = vec4(vs_color, 1.f);
+  vec3 ambientFinal = calculateAmbient(material);
+  vec3 diffuseFinal = calculateDiffuse(material, vs_position, vs_normal, LightPos0);
+  vec3 specularFinal = calculateSpecular(material, vs_position, vs_normal, LightPos0, CameraPos);
 
   //attenuation
 
   //final
   fs_color = texture(material.diffudeTex, vs_texcoord) * //texture(texture1, vs_texcoord) *
-			 (vec4(ambientLight, 1.f) + vec4(diffuseFinal, 1.f) + vec4(specularFinal, 1.f));
+             (vec4(ambientFinal, 1.f) + vec4(diffuseFinal, 1.f) + vec4(specularFinal, 1.f));
 }
